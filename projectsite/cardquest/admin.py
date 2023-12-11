c8f182037c16f6552d45628819e7c0df71f0003c
MichaelJoshuaCalan <mr.swabe.xx12345@gmail.com>
2023-12-11 15:00:29 +0800

    cq

---
+++ projectsite/cardquest/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import PokemonCard
+
+@admin.register(PokemonCard)
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ("name", "rarity")
+    search_fields = ("name",)
